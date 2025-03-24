473c8b4db355fe6d9003c06c2032e4779996b318
Sameed Tariq <49558508+tariqsameed@users.noreply.github.com>
2025-03-24 19:49:28 +0500

    Deleting Item as Code coverage is not present.

---
+++ src/main/java/com/localite/ecommerce/dto/Item.java
@@ -0,0 +1,16 @@
+package com.localite.ecommerce.dto;
+
+public class Item {
+
+    Long bookId;
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+
+}
