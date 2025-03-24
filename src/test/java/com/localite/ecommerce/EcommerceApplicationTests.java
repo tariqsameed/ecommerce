eebdde69617b6d94e23f0ccfe034153d49dbd72d
Sameed Tariq <49558508+tariqsameed@users.noreply.github.com>
2025-03-24 17:22:43 +0500

    Sample Spring boot Project

---
+++ src/test/java/com/localite/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.localite.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Noncompliant - method is empty
+	}
+
+}
