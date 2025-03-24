f84dd828e418cd5058991015ebd09021272fc684
Sameed Tariq <49558508+tariqsameed@users.noreply.github.com>
2025-03-24 17:40:06 +0500

    Item Object

---
+++ src/main/java/com/localite/ecommerce/dto/Item.java
@@ -0,0 +1,14 @@
+package com.localite.ecommerce.dto;
+
+public class Item {
+
+    Long bookId;
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+}
